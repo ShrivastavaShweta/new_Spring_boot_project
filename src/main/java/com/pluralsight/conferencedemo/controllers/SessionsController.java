ab8879f554b2bddfad1b9929a00247b95bc92244
Shweta Shrivastava <shweta.shrivastav@gmail.com>
2023-08-15 17:37:26 -0500

    added CRUD methods to session controller

--- src/main/java/com/pluralsight/conferencedemo/controllers/SessionsController.java
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SessionsController.java
@@ -4,6 +4,7 @@ import com.pluralsight.conferencedemo.models.Session;
 import com.pluralsight.conferencedemo.models.Speaker;
 import com.pluralsight.conferencedemo.repositories.SessionRepository;
 import com.pluralsight.conferencedemo.repositories.SpeakerRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -29,10 +30,22 @@ public class SessionsController {
         return sessionRepository.getOne(id);
     }
 
-//    @PostMapping
-//    @ResponseStatus(HttpStatus.CREATED)
-//    public Session create(@RequestBody final Session session){
-//        return sessionRepository.saveAndFlush(session);
-//    }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Session create(@RequestBody final Session session){
+        return sessionRepository.saveAndFlush(session);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id){
+        sessionRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Session update(@PathVariable Long id, @RequestBody Session session){
+        Session existingSession = sessionRepository.getOne(id);
+        BeanUtils.copyProperties(session,existingSession,"session_id");
+        return sessionRepository.saveAndFlush(existingSession);
+    }
 
 }
